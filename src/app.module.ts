9cca5540275e87daeb64a06173469f684eb74ef3
reikialw <reikialw@gmail.com>
2026-02-08 13:37:05 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User],
+      synchronize: true, // apenas para estudo
+    }),
+    UserModule,
+  ],
+})
+export class AppModule {}
