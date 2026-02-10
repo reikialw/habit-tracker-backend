b18d8d6b09843dca1a3871efcb7ca49e3edf1ed8
reikialw <reikialw@gmail.com>
2026-02-10 14:56:39 -0300

    create habit module (service, controller, module)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
 import { UserModule } from './user/user.module';
+import { HabitModule } from './habit/habit.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UserModule } from './user/user.module';
       entities: [User],
       synchronize: false,
     }),
+    HabitModule,
   ],
 })
 export class AppModule {}
