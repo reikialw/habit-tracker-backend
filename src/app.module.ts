ce616dc81994fd4c621fa3e7d56d90b678f0de48
reikialw <reikialw@gmail.com>
2026-02-10 14:29:05 -0300

    change user setup to use migrations of typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { UserModule } from './user/user.module';
       type: 'sqlite',
       database: 'db.sqlite',
       entities: [User],
-      synchronize: true, // apenas para estudo
+      synchronize: false,
     }),
   ],
 })
