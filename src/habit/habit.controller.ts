941ae4dae6a7fd84fdb0c4c247c60963faa25d64
reikialw <reikialw@gmail.com>
2026-02-14 10:05:32 -0300

    create controller for habit

--- src/habit/habit.controller.ts
+++ src/habit/habit.controller.ts
@@ -1,4 +1,20 @@
 import { Controller } from '@nestjs/common';
+import { Get, Post, Patch, Body } from '@nestjs/common';
+import { HabitService } from './habit.service';
+import { CreateHabitDto } from './dto/create-habit.dto';
 
 @Controller('habit')
-export class HabitController {}
+export class HabitController {
+
+    constructor(private readonly habitService: HabitService) {}
+
+    @Post()
+    create(@Body() dto: CreateHabitDto) {
+        return this.habitService.create(dto);
+    }
+
+    @Get()
+    findAll() {
+        return this.habitService.findAll();
+    }
+}
